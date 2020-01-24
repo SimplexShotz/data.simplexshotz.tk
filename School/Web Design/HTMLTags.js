73cac8d8b6f840092a3db333b43d24153fbadd38
SimplexShotz <44213379+SimplexShotz@users.noreply.github.com>
2020-01-23 23:40:56 -0500

    add some more 3.2 tags; see TODO comments in future

--- School/Web Design/HTMLTags.js
+++ School/Web Design/HTMLTags.js
@@ -97,6 +97,7 @@ var allTags = [
         }
     ], // ^ HTML 1.0 Tags ^
     [ // HTML 2.0 Tags
+        // TODO: Add TABLES; see http://www.martinrinehart.com/frontend-engineering/engineers/html/html-tag-history.html for more that may have bee missed
         {
             tag: "html",
             name: "HTML Tag",
@@ -314,7 +315,7 @@ var allTags = [
             isOld: false
         }
     ], // ^ HTML 2.0 Tags ^
-    [
+    [ // HTML 3.2 Tags
         { // TODO
             tag: "html",
             name: "HTML Tag",
@@ -361,30 +362,265 @@ var allTags = [
             link: "",
             isOld: null
         }, { // TODO
-            tag: "",
+            tag: "body",
             name: "",
             link: "",
             isOld: null
         }, { // TODO
-            tag: "",
+            tag: "h1-h6",
             name: "",
             link: "",
             isOld: null
         }, { // TODO
-            tag: "",
+            tag: "address",
             name: "",
             link: "",
             isOld: null
         }, { // TODO
-            tag: "",
+            tag: "p",
             name: "",
             link: "",
             isOld: null
         }, { // TODO
-            tag: "",
+            tag: "ul",
             name: "",
             link: "",
             isOld: null
-        }, 
-    ]
+        }, { // TODO
+            tag: "ol",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "li",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "dl",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "dt",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "dd",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "dir",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "menu",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "pre",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "div",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "center",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "blockquote",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "form",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "input",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "select",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "textarea",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "hr",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "table",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "tr",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "th",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "td",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "caption",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "tt",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "i",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "b",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "u",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "strike",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "big",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "small",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "sub",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "sup",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "em",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "strong",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "dfn",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "code",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "samp",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "kbd",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "var",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "cite",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "a",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "img",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "applet",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "param",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "font",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "basefont",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "br",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "map",
+            name: "",
+            link: "",
+            isOld: null
+        }, { // TODO
+            tag: "area",
+            name: "",
+            link: "",
+            isOld: null
+        }
+    ] // ^ HTML 3.2 Tags ^
 ];
